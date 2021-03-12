2154b70ee4d380287d6a7830940931ab4de79433
Ravisher Singh <60622194+ravishersingh2@users.noreply.github.com>
2021-03-12 14:57:56 -0700

    new

---
+++ movies.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise= global.Promise;
+
+mongoose.connect(process.env.DB,{useNewUrlParser:true});
+mongoose.set('useCreateIndex',true);
+
+var moviesSchema = new Schema({
+    title:{type:String,required:true,index:{unique:true}},
+    yearReleased:{type:Date, required:true},
+    genre:{type:String,required:true,enum:['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'] },
+    actors: { type: [{actorName: String, characterName: String}], required: true }
+});
+module.exports = mongoose.model('Movie',MovieSchema);
